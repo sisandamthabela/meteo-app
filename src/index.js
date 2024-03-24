aed2b4f8d02fc6c0033f4daa9573e9f7744af00f
sisandamthabela <mthabelasisanda@gmail.com>
2024-03-24 12:41:50 +0200

    created index.js file added basic js then api integration

---
+++ src/index.js
@@ -0,0 +1,24 @@
+function updateWeather(response) {
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "7a60d36485to64ac27f9bab0d1cc4ca3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+searchCity("Johannesburg");
